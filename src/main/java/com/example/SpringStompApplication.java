3704767d3a343c6b00f9b90270e2a655f7888af1
Arpit Aggarwal <arpitaggarwal@Arpits-MacBook-Pro.local>
2022-05-22 22:03:47 +0400

    first commit

--- src/main/java/com/example/SpringStompApplication.java
+++ src/main/java/com/example/SpringStompApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.config")
 public class SpringStompApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.dto.Greeting;
+import com.example.dto.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    @GetMapping("/greeting")
+    public String greeting() throws Exception {
+        return "Hello ";
+    }
+}
